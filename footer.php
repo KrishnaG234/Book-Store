2686374a85d87565bb163c388a6ca471752a4083
Krishna <gargkrishna828@gmail.com>
2024-10-02 18:08:38 +0530

    Initial commit

---
+++ footer.php
@@ -0,0 +1,40 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>Quick Links</h3>
+         <a href="home.php">Home</a>
+         <a href="about.php">About</a>
+         <a href="shop.php">Shop</a>
+         <a href="contact.php">Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra Links</h3>
+         <a href="login.php">Login</a>
+         <a href="register.php">Register</a>
+         <a href="cart.php">Cart</a>
+         <a href="orders.php">Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact Info</h3>
+         <p> <i class="fas fa-phone"></i> +123-456-7890 </p>
+         <p> <i class="fas fa-phone"></i> +111-222-3333 </p>
+         <p> <i class="fas fa-envelope"></i> bookly@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Noida, India - 201309 </p>
+      </div>
+
+      <div class="box">
+         <h3>Follow Us</h3>
+         <a href="https://www.facebook.com/BooksForAllInitiative/"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="https://twitter.com/i/topics/847519659179954178"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="https://www.instagram.com/getbetterwithbooks/?hl=en"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="https://www.linkedin.com/news/story/books-you-should-read-this-summer-1390066/"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+
+</section>
